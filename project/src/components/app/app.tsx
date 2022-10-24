c74389dfbfaf3f6130e0812cad97981448e0c84e
idKoala <komachenko.a@gmail.com>
2022-10-24 21:35:57 +0300

    Добавлены базовые маршруты к компонентам Main, Favorites, Login, Offer_id, NotFound

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,11 +1,44 @@
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import FavoritesScreen from '../../pages/favorites-screen/favorites-screen';
+import PlaceScreen from '../../pages/place-screen/place-screen';
+import LoginScreen from '../../pages/login-screen/login-screen';
 import MainScreen from '../../pages/main-screen/main-screen';
+import NotFound404Screen from '../../pages/not-found-404-screen/not-found-404-screen';
+
 
 type AppScreenProps = {
   placesCount: number;
 }
 
 function App({placesCount}: AppScreenProps): JSX.Element {
-  return <MainScreen placesCount={placesCount} />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path='/'
+          element={<MainScreen placesCount={placesCount}/>}
+        />
+      </Routes>
+      <Routes>
+        <Route
+          path='login'
+          element={<LoginScreen />}
+        />
+        <Route
+          path='favorites'
+          element={<FavoritesScreen />}
+        />
+        <Route
+          path='offer/:id'
+          element={<PlaceScreen />}
+        />
+        <Route
+          path='*'
+          element={<NotFound404Screen />}
+        />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
