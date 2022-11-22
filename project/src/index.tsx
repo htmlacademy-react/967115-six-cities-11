faa1ba1f75834288d79eaa4e9e52432d37957b66
idKoala <komachenko.a@gmail.com>
2022-11-22 23:18:35 +0500

    Инициализировано новое хранилище. App обернут в провайдер. Типизирован State и диспатч

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/app/app';
 import { offers } from './mocks/offers';
+import { Provider } from 'react-redux';
+import { store } from './store';
 
 const Setting = {
   PlacesCount: 45
@@ -13,9 +15,11 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
-    <App
-      placesCount={Setting.PlacesCount}
-      offers={offers}
-    />
+    <Provider store={store}>
+      <App
+        placesCount={Setting.PlacesCount}
+        offers={offers}
+      />
+    </Provider>
   </React.StrictMode>,
 );
