7c1d253e0f504ff6eada1e0b2ae6324778fe8be0
idKoala <komachenko.a@gmail.com>
2022-11-28 21:47:05 +0500

    Добавлены корректные координаты и зум в массив с городами

--- project/src/constants.ts
+++ project/src/constants.ts
@@ -18,17 +18,17 @@ export const CITIES: City[] = [
   {
     name: 'Paris',
     location: {
-      latitude: 52.3909553943508,
-      longitude: 4.85309666406198,
-      zoom: 11
+      latitude: 48.85661,
+      longitude: 2.351499,
+      zoom: 13
     },
   },
   {
     name: 'Cologne',
     location: {
-      latitude: 52.3909553943508,
-      longitude: 4.85309666406198,
-      zoom: 11
+      latitude: 50.938361,
+      longitude: 6.959974,
+      zoom: 13
     },
   },
   {
@@ -42,25 +42,25 @@ export const CITIES: City[] = [
   {
     name: 'Amsterdam',
     location: {
-      latitude: 52.3909553943508,
-      longitude: 4.85309666406198,
-      zoom: 11
+      latitude: 52.37454,
+      longitude: 4.897976,
+      zoom: 13
     },
   },
   {
     name: 'Hamburg',
     location: {
-      latitude: 52.3909553943508,
-      longitude: 4.85309666406198,
-      zoom: 11
+      latitude: 53.550341,
+      longitude: 10.000654,
+      zoom: 13
     },
   },
   {
     name: 'Dusseldorf',
     location: {
-      latitude: 52.3909553943508,
-      longitude: 4.85309666406198,
-      zoom: 11
+      latitude: 51.225402,
+      longitude: 6.776314,
+      zoom: 13
     },
   },
 ];
